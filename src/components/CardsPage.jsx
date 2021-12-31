f1bbcc3bf84e106f1acdaabce682259cdc559b83
Pio Saija <71008079+Pio-js@users.noreply.github.com>
2021-12-31 13:05:59 +0100

    Update CardsPage.jsx

--- src/components/CardsPage.jsx
+++ src/components/CardsPage.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import Hotels from './Hotels';
 import './css/Card.css';
 
@@ -9,19 +8,14 @@ export default function CardsPage(props) {
     const adult = props.adult;
     const children = props.children;
 
-    const [cards, setCards] = useState();
-
-    useEffect(() => {
-        if(data.length > 0 && rooms.length > 0){
-            setCards(<Hotels hotelData={data} roomsData={rooms} adult={adult} children={children}/>);
-        }else{
-            setCards(<h1>No hotels found</h1>);
-        }
-    }, [data, rooms, adult, children]);
-
     return (
         <div id='card-page'>
-            {cards}
+            {
+            data.length > 0 && rooms.length > 0 ?
+            <Hotels hotelData={data} roomsData={rooms} adult={adult} children={children}/>
+            :
+            <h1>No hotels found</h1>
+            }
         </div>
     )
 }
